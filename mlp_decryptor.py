8f533595835e69046969a69309ce90767e97f29f
Undyne_The_Luka <Zomial@yandex.ru>
2017-06-25 18:05:41 +0300

    Initial commit

---
+++ mlp_decryptor.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# Шифр Гладина - шифр, которым зашифрованы многие скрытые сообщения в его пупах (которые он выпускает под ником Multiprogramm).
+# В шифре присутствуют только латинские буквы, регистр не важен. В зашифрованном виде они будут представлять трёхзначное число.
+
+def decryptChar (_dict, _val):
+    for k, v in _dict.items():
+        if v == _val:
+            return k
+    return _val
+
+crypt = {
+    'A': '394',
+    'B': '110',
+    'C': '321',
+    'D': '846',
+    'E': '271',
+    'F': '730',
+    'G': '981',
+    'H': '401',
+    'I': '237',
+    'J': '903',
+    'K': '845',
+    'L': '101',
+    'M': '902',
+    'N': '900',
+    'O': '197',
+    'P': '314',
+    'Q': '333',
+    'R': '111',
+    'S': '205',
+    'T': '414',
+    'U': '722',
+    'V': '100',
+    'W': '177',
+    'X': '911',
+    'Y': '448',
+    'Z': '999',
+         }
+
+def cryptString(string):
+    string = string.upper()
+    encrypted = ''
+    for char in string:
+        encrypted += crypt.get(char, char);
+    return encrypted
+
+def decryptString(string):
+    string = string.upper()
+    decrypted = ''
+    while string[:3] != '':
+        decrypted += decryptChar(crypt, string[:3])
+        string = string[3:]
+    return decrypted
+
+print ("Эта программа шифрует и дешифрует текст т. н. шифром Гладина.")
+print ("Выбрать первое действие (шифровка) можно, написав число 1. Второе (дешифровка) - написав число 2.")
+print ("Выберите нужное действие.")
+choice = int(input())
+if (choice == 1):
+    print('Введите фразу, требующую шифровки.')
+    S = input()
+    encrypted = cryptString(S)
+    print('Фраза зашифрована. Результат:')
+    print(encrypted)
+elif (choice == 2):
+    print('Введите зашифрованную фразу, требующую расшифровки.')
+    S = input()
+    decrypted = decryptString(S)
+    print('Фраза расшифрована. Результат:')
+    print(decrypted)
+else:
+    print('Неверный ввод')
+
+
+
